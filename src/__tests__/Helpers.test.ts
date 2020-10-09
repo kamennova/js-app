16114d84622320fd1d86ce4ee94dee2b68886df5
Masha <mashushvik.2001@gmail.com>
2020-10-09 15:49:22 +0300

    feat(tests): add line

--- src/__tests__/Helpers.test.ts
+++ src/__tests__/Helpers.test.ts
@@ -2,6 +2,7 @@ import { formatMoney } from "../helpers";
 
 test("it should format money properly", () => {
     expect(formatMoney(0)).toBe('$0');
+    expect(formatMoney(10)).toBe('$10');
     expect(formatMoney(500)).toBe('$500');
     expect(formatMoney(1000)).toBe('$1,000');
     expect(formatMoney(200000)).toBe('$200,000');
